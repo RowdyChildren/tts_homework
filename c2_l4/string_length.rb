1f2def63dcc46cf7547edb5dd0aa4aefdc8c07a5
Riley Childs <riley@tfsgeo.com>
2014-11-06 10:27:52 -0500

    Added Command line args to string counters

--- c2_l4/string_length.rb
+++ c2_l4/string_length.rb
@@ -1,4 +1,9 @@
 #Homework Class 2
-puts "Enter a String"
-string = gets.chomp
+if ARGV[0] == nil
+	puts "Sorry, you need to specify a Command Line Argument in the form string_length.rb [STRING]"
+	exit
+else
+string = ARGV[0].chomp
 puts "Your string is #{string.length} Chars long"
+end
+exit
