fdefaeec7ea8f4fdf8407fbb58897721ad1df7df
Riley Childs <riley@tfsgeo.com>
2014-11-06 10:49:01 -0500

    More Comments!

--- c2_l4/pounds_to_kilos.rb
+++ c2_l4/pounds_to_kilos.rb
@@ -1,10 +1,19 @@
 #Class 2 Homework
+#Define Function
 def convert_pounds_to_kilos(number)
+	#make Conversion
 	mass_kilos = number * 0.453592
+	#return value
 	return mass_kilos
 
 end
-puts "gimmie a value in pounds"
+#Tell User What to do
+puts "gimmie a value in pounds, decimals will be removed"
+#Get User Input and covert to interger
 weight = gets.chomp.to_i
+#call function to do conversion
 mass = convert_pounds_to_kilos(weight)
+#tell user the conversion value
 puts "Your value has a mass of #{mass.to_s} kg"
+#Terminate Gracefully
+exit
