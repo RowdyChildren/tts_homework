1174ef5f0647bb263460a0a8c2beaf231ad4bd6d
Riley Childs <riley@tfsgeo.com>
2014-11-06 10:33:17 -0500

    added more comments

--- c2_l4/string_length.rb
+++ c2_l4/string_length.rb
@@ -1,9 +1,16 @@
 #Homework Class 2
+#Check args
 if ARGV[0] == nil
+#Oops need to spec an arg
 	puts "Sorry, you need to specify a Command Line Argument in the form string_length.rb [STRING]"
 	exit
 else
+	#good job! You passed an arg to the program
 string = ARGV[0].chomp
+#Lets tell the user what the string is
 puts "Your string is #{string.length} Chars long"
+#terminate gracefully
+exit
 end
+#how'd ya get here
 exit
